9623416a856c801c40893bcc3d7308a098ab427e
Mathieu Geukens <mr-zwets@protonmail.com>
2023-10-21 14:37:28 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  esbuild: {
+    supported: {
+      // Browsers can handle top-level-await features.
+      'top-level-await': true
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        collection: resolve(__dirname, 'collection.html'),
+        404: resolve(__dirname, '404.html'),
+      },
+    },
+  },
+});
